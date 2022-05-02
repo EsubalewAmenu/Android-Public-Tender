77cb9911fb138b27d680ca0894416dcdbdd756eb
Esubalew Amenu <esubalew.a2009@gmail.com>
2022-05-02 12:07:34 +0300

    Filter fragment [not working]

--- app/src/main/java/rise/africa/apps/publictender/MainActivity.java
+++ app/src/main/java/rise/africa/apps/publictender/MainActivity.java
@@ -1,12 +1,19 @@
 package rise.africa.apps.publictender;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -14,30 +21,25 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import rise.africa.apps.publictender.databinding.ActivityMainBinding;
+import rise.africa.apps.publictender.filter.EcommerceFiltersFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+
+    FragmentTransaction transaction;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -47,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+
+
+        fragment = new EcommerceFiltersFragment();
     }
 
     @Override
@@ -55,11 +62,56 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                showFragment();
+                return true;
+//            case R.id.action_rate_us:
+//                Toast.makeText(MainActivity.this, "Rate this app :)", Toast.LENGTH_SHORT).show();
+//                rateApp();
+//                return true;
+//            case R.id.action_app_store:
+//                Toast.makeText(MainActivity.this, "More apps :)", Toast.LENGTH_SHORT).show();
+//                openUrl("https://play.google.com/store/apps/developer?id=Herma%20plc");
+//                return true;
+//            case R.id.action_about:
+//                startActivity(new Intent(getApplicationContext(), About_us.class));
+//                return true;
+//            case R.id.action_exit:
+//                System.exit(0);
+//                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void dismissFragment() {
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        fragmentManager.popBackStackImmediate();
+        onBackPressed();
+    }
+
+    private void showFragment() {
+        transaction = getSupportFragmentManager().beginTransaction();
+        if (!fragment.isAdded()) {
+            transaction.add(R.id.frame, fragment, "filter");
+        }
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/rise/africa/apps/publictender/shared/DB.java
@@ -0,0 +1,132 @@
+package rise.africa.apps.publictender.shared;
+
+/*
+ * Created by Esubalew Amenu on 04-Jan-19
+ * Mobile +251 92 348 1783
+ * Email esubalew.a2009@gmail.com/
+ */
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DB extends SQLiteOpenHelper {
+
+	Context context;
+	static String DB_NAME =  "pt.hrm";
+	SQLiteDatabase myDataBase;
+
+	public DB(Context context) {
+		super(context, DB_NAME, null, 1 /* This is the version of the database*/);
+		this.context = context;
+
+		myDataBase = this.getReadableDatabase();
+
+		onCreate(myDataBase);
+
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		System.out.println("called from db on create");
+		boolean dbexist = checkdatabase();
+		if (dbexist) {
+			//System.out.println("Database exists");
+			opendatabase();
+		} else {
+			System.out.println("Database doesn't exist");
+			try {
+				createdatabase();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		System.out.println("called from db on update");
+
+		try {
+			System.out.println("db delete is deleted");
+			File file = file = new File(context.getFilesDir().getPath()+"/"  + DB_NAME);
+			if (file.exists()) file.delete();
+
+
+			createdatabase();
+			Toast.makeText(context, "Application Updated! please restart the app", Toast.LENGTH_SHORT).show();
+
+		} catch (Exception ds) {
+			System.out.println("db delete is on exception " +ds);
+		}
+
+	}
+	public void opendatabase() throws SQLException {
+		String myPath = context.getFilesDir().getPath()+"/" + DB_NAME;
+		myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+		//SQLiteDatabase.OPEN_READONLY);
+	}
+
+	private boolean checkdatabase() {
+		//SQLiteDatabase checkdb = null;
+		boolean checkdb = false;
+		try {
+			String myPath = context.getFilesDir().getPath()+"/"+DB_NAME;
+			File dbfile = new File(myPath);
+			//checkdb = SQLiteDatabase.openDatabase(myPath,null,SQLiteDatabase.OPEN_READWRITE);
+			checkdb = dbfile.exists();
+		} catch(SQLiteException e) {
+			System.out.println("Database doesn't exist");
+		}
+		return checkdb;
+	}
+
+	public void createdatabase() throws IOException {
+		boolean dbexist = checkdatabase();
+		if(dbexist) {
+			//System.out.println(" Database exists.");
+		} else {
+			try {
+				copydatabase();
+			} catch(IOException e) {
+				throw new Error("Error copying database" + e);
+			}
+		}
+	}
+	private void copydatabase() throws IOException {
+		//Open your local db as the input stream
+		InputStream myinput = context.getAssets().open(DB_NAME);
+
+		//Open the empty db as the output stream
+		OutputStream myoutput = new FileOutputStream(context.getFilesDir().getPath()+"/"+DB_NAME);
+
+		// transfer byte to inputfile to outputfile
+		byte[] buffer = new byte[1024];
+		int length;
+		while ((length = myinput.read(buffer))>0) {
+			myoutput.write(buffer,0,length);
+		}
+
+		//Close the streams
+		myoutput.flush();
+		myoutput.close();
+		myinput.close();
+	}
+///////////////////////////////////////////////////////////////////////////////////
+public Cursor getSelect(String select, String from, String where) {
+	Cursor cursor = myDataBase.rawQuery("SELECT "+select+" FROM "+from+" WHERE "+ where, null);
+	return cursor;
+}
+
+}
